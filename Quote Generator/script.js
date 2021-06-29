889438673e99b50bc625178d64a41dd6265a8df5
Lee Maurin <leem4421@gmail.com>
2021-06-28 20:05:54 -0500

    change font size

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -10,7 +10,21 @@ let apiQuotes = [];
 function newQuote(){
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	authorText.textContent = quote.author;
+	// Check if Author field is blank and replace it with 'unknown'
+	if (!quote.author) {
+		authorText.textContent = 'unknown';
+	} else {
+		authorText.textContent = quote.author;
+	}
+
+	// Check Quote length to determine styling
+	if (quote.text.length > 50)  {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	quoteText.textContent = quote.text;
+
 	quoteText.textContent = quote.text;
 }
 
