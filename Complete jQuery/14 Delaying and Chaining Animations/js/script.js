5c538558af91917e8bc5c195ab5807bf8831eb1a
Lee Maurin <leem4421@gmail.com>
2021-08-01 18:36:10 -0500

    jQuery delay

--- Complete jQuery/14 Delaying and Chaining Animations/js/script.js
+++ Complete jQuery/14 Delaying and Chaining Animations/js/script.js
@@ -1,6 +1,5 @@
 $(function() {
-  // jQuery goes here...
-
-  // Uncomment this line to fade out the red box on page load
-   $(".red-box").fadeOut(2000);
+   $(".red-box").fadeTo(1000, 0.2);
+   $(".green-box").delay(1000).fadeTo(1000, 0.5);
+   $(".blue-box").delay(2000).fadeTo(1000, 0.8).delay(500).fadeIn();
 });
