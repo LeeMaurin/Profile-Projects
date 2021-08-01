c00f67579ac437e5fc7a1dcb10f31166373700d4
Lee Maurin <leem4421@gmail.com>
2021-08-01 18:24:18 -0500

    jQuery animate

--- Complete jQuery/13 Get Creative Custom Animations/js/script.js
+++ Complete jQuery/13 Get Creative Custom Animations/js/script.js
@@ -1,6 +1,13 @@
 $(function() {
-  // jQuery goes here...
-
-  // Uncomment this line to fade out the red box on page load
-   $(".red-box").fadeOut(2000);
+   $(".blue-box").animate({
+     "margin-left": "200px",
+     "opacity": "0",
+     "height": "50px",
+     "width": "50px",
+     "margin-top": "25px"
+   }, 1000);
+   
+   $("p").animate({
+     "font-size": "20px"
+   }, 1000)
 });
