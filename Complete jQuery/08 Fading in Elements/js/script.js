e4ce43ea735d5fb1b6a45738e88e29ad27864f84
Lee Maurin <leem4421@gmail.com>
2021-08-01 17:14:38 -0500

    jQuery fade

--- Complete jQuery/08 Fading in Elements/js/script.js
+++ Complete jQuery/08 Fading in Elements/js/script.js
@@ -2,5 +2,16 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-   $(".red-box").fadeOut(2000);
+  //  $(".red-box").fadeOut(2000);
+  //  $(".green-box").fadeOut(2000);
+  //  $(".blue-box").fadeOut(2000);
+  //  $(".red-box").fadeIn(2000);
+  //  $(".green-box").fadeIn(2000);
+  //  $(".blue-box").fadeIn(2000);
+   $(".red-box").fadeTo(1000, 0.2);
+   $(".green-box").fadeTo(3000, 0.5);
+   $(".blue-box").fadeTo(4000, 0.8);
+
+   $(".blue-box").fadeToggle(2000);
+   $(".blue-box").fadeToggle(2000);
 });
