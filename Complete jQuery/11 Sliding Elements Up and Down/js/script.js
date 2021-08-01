4c9234999e23013a002a60f4bbb0c0a06e27dfdc
Lee Maurin <leem4421@gmail.com>
2021-08-01 18:03:22 -0500

    jQuery slide

--- Complete jQuery/11 Sliding Elements Up and Down/js/script.js
+++ Complete jQuery/11 Sliding Elements Up and Down/js/script.js
@@ -2,5 +2,8 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-   $(".red-box").fadeOut(2000);
+   $(".blue-box").slideToggle(2000);
+   $(".blue-box").slideDown(2000);
+   $("p").hide();
+   $("p").slideDown(2000);
 });
