23e4d7042716ef10467a99ee7bd2a5b54da91efb
Lee Maurin <leem4421@gmail.com>
2021-08-01 19:04:37 -0500

    jQuery callback functions

--- Complete jQuery/15 Timing Animations with Callback Functions/js/script.js
+++ Complete jQuery/15 Timing Animations with Callback Functions/js/script.js
@@ -1,6 +1,5 @@
 $(function() {
-  // jQuery goes here...
-
-  // Uncomment this line to fade out the red box on page load
-   $(".red-box").fadeOut(2000);
+   $(".red-box").fadeTo(1000, 0, function() {
+     alert("This is the callback: Animation finished")
+   });
 });
