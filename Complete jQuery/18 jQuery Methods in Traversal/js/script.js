87c44f3493e1c0eaabcb62508cd4d07480858680
Lee Maurin <leem4421@gmail.com>
2021-08-04 09:13:40 -0500

    transversal final

--- Complete jQuery/18 jQuery Methods in Traversal/js/script.js
+++ Complete jQuery/18 jQuery Methods in Traversal/js/script.js
@@ -2,5 +2,6 @@ $(function() {
   // $("#list").find("li").css("background-color", "orange")
   // $("#list").children("li").css("background-color", "orange")
   // $("#list").parents("body").css("background-color", "orange")
-  $("#list").parents().css("background-color", "orange")
+  $(":header").next().css("background-color", "orange")
+  $("input").css("background-color", "orange")
 });
